d9eef6b70fd0e88ed80e56dfe247350d5dee3d17
Fonzie-byte <twistfons@live.nl>
2024-07-28 14:03:47 +0200

    WiP

--- ch02_01_literals_and_operators/src/main.rs
+++ ch02_01_literals_and_operators/src/main.rs
@@ -6,13 +6,13 @@ fn main() {
     // Scientific notation must be a float, so the following line doesn't work.
     // let power_level: i32 = 9e3;
 
-    // This isn't scientific notation cos `e` is a letter in hex (:
-    println!("9000 in hexadecimal is {}??", 9e3);
-
     // Bitwise operations
     println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101); // both bits true
     println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101); // either bit true
     println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101); // one bit true
-    println!("1 << 5 is {}", 1u32 << 5); // im too smooth brain
-    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2); // for this low-level stuff
+    println!("1 LEFT_SHIFT 5 is {}", 1u32 << 5); // im too smooth brain
+    println!("0x80 RIGHT_SHIFT 2 is 0x{:x}", 0x80u32 >> 2); // for this low-level stuff
+
+    // Underscores are 'ignored', to improve legibility.
+    println!("One miljard can be written as {}.", 1_000_000_000);
 }
